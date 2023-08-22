e4ea191f4d58921720c87b3077d4aabdeef37495
Jordan Owens <jordanowens1125@gmail.com>
2023-08-22 16:02:43 -0500

    added navbar boilerplate and scroll to top template

--- hero-section/2/scripts.js
+++ hero-section/2/scripts.js
@@ -1,14 +1,30 @@
 const clearAllBackgroundImages = () => {
   const wrapper = document.getElementById("img-wrapper");
   for (const child of wrapper.children) {
-    child.classList.remove("show");
-    child.classList.add("hide");
+    child.classList.remove("show-bg");
+    child.classList.add("hide-bg");
+  }
+};
+
+const clearAllSections = () => {
+  const wrapper = document.getElementById("content-sections");
+  for (const child of wrapper.children) {
+    child.classList.remove("show-section");
+    child.classList.add("hide-section");
   }
 };
 
 const setImg = (imgNum) => {
   clearAllBackgroundImages();
+  clearAllSections();
   const img = document.getElementById("img-wrapper").children[imgNum];
-  img.classList.remove("hide");
-  img.classList.add("show");
+  img.classList.remove("hide-bg");
+  img.classList.add("show-bg");
+  setSection(imgNum);
+};
+
+const setSection = (num) => {
+  const section = document.getElementById("content-sections").children[num];
+  section.classList.remove("hide-section");
+  section.classList.add("show-section");
 };
