751d37e8851d702a0224c9ba40bf6fc3520ffb3e
Jordan Owens <jordanowens1125@gmail.com>
2023-11-07 13:13:13 -0600

    new sections

--- design/grid-system/scripts.js
+++ design/grid-system/scripts.js
@@ -1,13 +1,13 @@
 const height = document.documentElement.clientHeight;
 
-const rows = height / 10;
+const rows = height / 16;
 
 const rowsEl = document.getElementById("rows");
 
 console.log(rows);
 
-for (let i = 0; i < rows; i++) {
-  const newRow = document.createElement("div");
-  newRow.classList.add("row");
-  rowsEl.appendChild(newRow);
-}
+// for (let i = 0; i < rows; i++) {
+//   const newRow = document.createElement("div");
+//   newRow.classList.add("row");
+//   rowsEl.appendChild(newRow);
+// }
